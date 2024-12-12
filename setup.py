b9d88f2a6bef28afaf15f91e3f7e5b9e571ecb40
akshaytheau <akshaytheau@gmail.com>
2024-12-12 10:31:55 +0530

    Changes for package renaming.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="prompt_evaluator",
-    version="0.1.2",
-    description="A package to evaluate prompts using OpenAI models",
+    name="prompt-evaluator",
+    version="0.1.8",
+    description="A package to evaluate prompts using OpenAI and google models",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Akshay Ugale",
     author_email="akshay.ugale88@gmail.com",
     url="https://github.com/akshaytheau/prompt_evaluator",
     packages=find_packages(),
-    install_requires=["openai"],
+    install_requires=["openai","tiktoken","google.generativeai"],
     python_requires=">=3.7",
     classifiers=[
         "Programming Language :: Python :: 3",
